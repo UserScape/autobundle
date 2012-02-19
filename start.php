ab12881be1497c9752409619cc20c2c4dfec7621
Ian Landsman <ian@userscape.com>
2012-02-18 20:54:26 -0500

    convert tabs to spaces for @speno :)

--- start.php
+++ start.php
@@ -8,32 +8,32 @@
  */
 
 array_map(function($path){
-	
-	// Only treat paths with start.php as proper bundle paths as all bundles must have start.php
-	if(strpos($path, 'start.php') !== false){
+    
+    // Only treat paths with start.php as proper bundle paths as all bundles must have start.php
+    if(strpos($path, 'start.php') !== false){
 
-		// Set the base path to the bundle
-		$path = dirname($path);
+        // Set the base path to the bundle
+        $path = dirname($path);
 
-		// Find the last directory name in path to use as bundle name
-		$name = basename($path);
+        // Find the last directory name in path to use as bundle name
+        $name = basename($path);
 
-		// If the bundle is not already registered then register it
-		if(!Bundle::exists($name)){
+        // If the bundle is not already registered then register it
+        if(!Bundle::exists($name)){
 
-			// Load the bundle
-			// If a routes file exists then we'll assume it handles routes of it's own name.
-			// Remember, if you need it to handle a custom route you should manually add
-			// the bundle in application/bundles.php.
-			Bundle::register($name, array(
-					'handles'	=> File::exists($path.DS.'routes.php') ? $name : null,
-					'location'	=> str_replace(path('bundle'), '', $path), 
-					'auto'		=> true)
-			);
+            // Load the bundle
+            // If a routes file exists then we'll assume it handles routes of it's own name.
+            // Remember, if you need it to handle a custom route you should manually add
+            // the bundle in application/bundles.php.
+            Bundle::register($name, array(
+                    'handles'   => File::exists($path.DS.'routes.php') ? $name : null,
+                    'location'  => str_replace(path('bundle'), '', $path), 
+                    'auto'      => true)
+            );
 
-			// autobundle is already in the loop that's starting bundles so we
-			// can't let the normal mechanism start it. We'll start it here.
-			Bundle::start($name);
-		}
-	}	
+            // autobundle is already in the loop that's starting bundles so we
+            // can't let the normal mechanism start it. We'll start it here.
+            Bundle::start($name);
+        }
+    }   
 }, iterator_to_array(new RecursiveIteratorIterator(new RecursiveDirectoryIterator(path('bundle')))));
